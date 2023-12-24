82a4af7378ac20df069d2fcb39713587c540a0de
Ekrem Can Lale <ekremlale1907@gmail.com>
2023-12-24 21:56:03 +0300

    header and footer

---
+++ frontend/mern e-commerce/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Policy from "./components/Policy";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>HELLO</h1>
+      <Policy />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
