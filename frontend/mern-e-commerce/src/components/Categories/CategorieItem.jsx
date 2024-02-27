89012e7cd1f86ee9ad82639d7d15cf7128f3d7bd
EkremL <ekremlale1907@gmail.com>
2024-02-28 02:35:32 +0300

    frontend-backend merge: Categories

--- frontend/mern-e-commerce/src/components/Categories/CategorieItem.jsx
+++ frontend/mern-e-commerce/src/components/Categories/CategorieItem.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./CategorieItem.css";
 
-const CategorieItem = () => {
+const CategorieItem = ({ category }) => {
   return (
     <li className="category-item">
       <a href="#">
-        <img
-          src="img/categories/categories1.png"
-          alt=""
-          className="category-image"
-        />
-        <span className="category-title">Smartphone</span>
+        <img src={category.img} alt="" className="category-image" />
+        <span className="category-title">{category.name}</span>
       </a>
     </li>
   );
 };
 
 export default CategorieItem;
+
+CategorieItem.propTypes = {
+  category: PropTypes.object,
+};
--- frontend/mern-e-commerce/src/components/Categories/Categories.jsx
+++ frontend/mern-e-commerce/src/components/Categories/Categories.jsx
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CategorieItem from "./CategorieItem";
+import { message } from "antd";
 import "./Categories.css";
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/api/categories`);
+
+        if (response.ok) {
+          const data = await response.json();
+          setCategories(data);
+        } else {
+          message.error("Çekme işlemi başarisiz");
+        }
+      } catch (error) {
+        console.log("Çekme hatasi", error);
+      }
+    };
+    fetchCategories();
+  }, [apiUrl]);
+
   return (
     <section className="categories">
       <div className="container">
@@ -12,12 +33,15 @@ const Categories = () => {
           <p>Summer Collection New Morden Design</p>
         </div>
         <ul className="category-list">
+          {/* <CategorieItem />
           <CategorieItem />
           <CategorieItem />
           <CategorieItem />
-          <CategorieItem />
-          <CategorieItem />
-          <CategorieItem />
+          <CategorieItem /> */}
+          {/* artik fetch ile kategorileri çekiyoruz  */}
+          {categories.map((category) => (
+            <CategorieItem key={category._id} category={category} />
+          ))}
         </ul>
       </div>
     </section>
